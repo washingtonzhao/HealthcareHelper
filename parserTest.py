7eabe974642d0cdca3fb3dbe8b15c4821f98eda1
Washington <washingz@usc.edu>
2019-03-02 14:11:27 -0800

    Finished parser of database, don't need hard code

--- parserTest.py
+++ parserTest.py
@@ -1,10 +1,12 @@
 import mechanicalsoup
 from bs4 import BeautifulSoup
 import urllib3
+import certifi
 
-searchQuery = "19543X Blood test -- cholesterol lipids"
-zipCode = 90089
-radius = "5"
+
+searchQuery = str(input("What are you searching for?\n"))
+zipCode = int(input("What is your zipcode?\n"))
+radius = int(input("What mile radius? (5,10,25,50,100,500)\n"))
 
 browser = mechanicalsoup.StatefulBrowser()
 
@@ -16,18 +18,31 @@ browser["query"] = searchQuery
 browser["zip_code"] = zipCode
 browser["radius"] = radius
 
-browser.launch_browser()
+##browser.launch_browser()
 
 response = browser.submit_selected()
 
-print(browser.get_url())
+##print(browser.get_url())
 
-http = urllib3.PoolManager()
+http = urllib3.PoolManager(cert_reqs ='CERT_REQUIRED',ca_certs = certifi.where())
 response = http.request('GET', browser.get_url())
+soup = BeautifulSoup(response.data,'lxml')
 
-soup = BeautifulSoup(response.data)
+results = soup.find('span', 'ui orange circular label')
 
-## need to be able to get multiple addresses
-name_box = soup.find_all('span', attrs={'class': 'address'},recursive=False)
-for name in name_box:
-    print(name)
+
+numResults = int(results.text.strip())
+##print(numResults)
+
+if numResults > 0: 
+    prices = soup.findAll('div',attrs={'class','price-badge price-charged'})
+    addresses = soup.findAll('span', attrs={'itemprop', 'address'})
+
+    for price in prices:
+        print(price.text.strip())
+
+    for address in addresses:
+        print(address.text.strip())
+
+else:
+    print("Sorry, there were no reported results for prices in our database.")
