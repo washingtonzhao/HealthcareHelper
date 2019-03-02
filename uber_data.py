4c1d1103353458316393a578074591f1cdaf2e01
Kevin Zhu <zhu.kevin@berkeley.edu>
2019-03-02 03:42:18 -0800

    added list of hospitals for blood tests and mammograms

--- uber_data.py
+++ uber_data.py
@@ -1,34 +1,21 @@
 from flask import Flask
 from flask import render_template
+from uber_data_helper import getData
 
 app = Flask(__name__)
 @app.route('/')
 
+def data():
+	blood_test_data = [[34.2048, -118.2158, "btest"],[34.0547, -118.2653, "btest"], [34.0464, -118.2605, "btest"], [34.0633, -118.2387, "mammogram"], [34.0528, - 118.2640, "mammogram"], [34.1181, -118.1511, "mammogram"]]
+	blood_test = ["USC Verdugo Hills Hospital", "Good Samaritan Hospital", " Keck Medicine – Downtown Los Angeles"]
+	b_test_out = []
 
+	for hospital in blood_test_data:
+		b_test_out.append(getData(hospital[0], hospital[1], hospital[2]))
+	
 
-def getData():
-	import json
-	from uber_rides.session import Session
-	from uber_rides.client import UberRidesClient
-	session = Session(server_token="rLvqhp6YAeoa_DS20C_ROun-zvcBw5wzr9AageOL")
-	client = UberRidesClient(session)
-	response = client.get_price_estimates(
-    	start_latitude=34.140,
-    	start_longitude=-118.124,
-    	end_latitude=34.134,
-    	end_longitude=-118.152,
-    	seat_count=2
-    	)
-
-	estimate = response.json.get('prices')
-
-
-	cost = estimate[4]["estimate"]
-	rideDuration = estimate[4]["duration"]//60
-
-	out = {"cost": cost, "rideDuration": rideDuration}
-
-	return render_template("index.html", name=out)
+	
+	return render_template("index.html", name=b_test_out)
 
 if __name__ == "__main__":
 	app.run()
