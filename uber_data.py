d1df185171efd72c9a8081e057c4d2e72f7b9f5f
Kevin Zhu <zhu.kevin@berkeley.edu>
2019-03-02 02:50:18 -0800

    updated it so that it creates a local server and sends uber data

--- uber_data.py
+++ uber_data.py
@@ -1,19 +1,37 @@
-import json
-from uber_rides.session import Session
+from flask import Flask
+from flask import render_template
 
-session = Session(server_token="rLvqhp6YAeoa_DS20C_ROun-zvcBw5wzr9AageOL")
+app = Flask(__name__)
+@app.route('/')
 
 
-from uber_rides.client import UberRidesClient
-client = UberRidesClient(session)
-response = client.get_price_estimates(
-    start_latitude=34.140,
-    start_longitude=-118.124,
-    end_latitude=34.134,
-    end_longitude=-118.152,
-    seat_count=2
-)
 
-estimate = response.json.get('prices')
+def getData():
+	import json
+	from uber_rides.session import Session
+	from uber_rides.client import UberRidesClient
+	session = Session(server_token="rLvqhp6YAeoa_DS20C_ROun-zvcBw5wzr9AageOL")
+	client = UberRidesClient(session)
+	response = client.get_price_estimates(
+    	start_latitude=34.140,
+    	start_longitude=-118.124,
+    	end_latitude=34.134,
+    	end_longitude=-118.152,
+    	seat_count=2
+    	)
 
-print(estimate[0]["estimate"])
+	estimate = response.json.get('prices')
+
+
+	cost = estimate[4]["estimate"]
+	rideDuration = estimate[4]["duration"]//60
+
+	out = {"cost": cost, "rideDuration": rideDuration}
+
+	return render_template("index.html", name=out)
+
+if __name__ == "__main__":
+	app.run()
+
+
+
